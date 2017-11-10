c24fef821929f8fe93d03940a3d36d372924a61c
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2017-11-09 19:21:49 -0500

    Final solution

--- AC-iOS-StackProblems/StackProblems.swift
+++ AC-iOS-StackProblems/StackProblems.swift
@@ -9,18 +9,34 @@
 import Foundation
 
 
-
 //Problem One:
 //Find the largest integer in a Stack of Ints
 func largestElement<T: Comparable>(in stack: Stack<T>) -> T? {
-    return stack.peek()
+    var stack = stack
+    var largestStack = stack.peek()
+    while !stack.isEmpty {
+        if stack.peek()! > largestStack! {
+            largestStack = stack.pop()!
+        } else {
+            let _ = stack.pop()
+        }
+    }
+    return largestStack
 }
 
 
 //Problem Two:
 //Find the sum of a Stack of Ints
-func sum(of stack: Stack<Int>) -> Int {
-    return 0
+func sum(of stack: Stack<Int>) -> Int? {
+    var stack = stack
+    var stackSum = stack.pop()
+    while !stack.isEmpty {
+        stackSum = stackSum! + stack.pop()!
+    }
+    guard let sum = stackSum else {
+        return nil
+    }
+    return sum
 }
 
 //Problem Three:
@@ -42,21 +58,43 @@ func sum(of stack: Stack<Int>) -> Int {
  */
 
 func reversed<T>(stack: Stack<T>) -> Stack<T> {
-    return Stack<T>()
+    var stack = stack
+    var newStack: Stack<T> = Stack()
+    
+    while !stack.isEmpty {
+        newStack.push(stack.pop()!)
+    }
+    
+    return newStack
 }
 
 
 //Problem Four:
 //Determine if two stacks are equal
 func equalStacks<T: Equatable>(stackOne: Stack<T>, stackTwo: Stack<T>) -> Bool {
-    return false
+    var stackOne = stackOne
+    var stackTwo = stackTwo
+    while !stackOne.isEmpty && !stackTwo.isEmpty {
+        if stackOne.pop() == stackTwo.pop() {
+            continue
+        } else {
+            return false
+        }
+    }
+    return true
 }
 
 
 //Problem Five:
 //Write a function that pushes a new element to the bottom of a Stack
 func pushBottom<T>(stack: Stack<T>, newElement: T) -> Stack<T> {
-    return Stack<T>()
+    var stack = stack
+    var newStack: Stack<T> = Stack()
+    newStack.push(newElement)
+    while !stack.isEmpty {
+        newStack.push(stack.pop()!)
+    }
+    return newStack
 }
 
 //Problem Six:
